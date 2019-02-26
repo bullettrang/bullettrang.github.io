5db8dd50a7d9127b2e78258c623643b365772a3c
Your Name <btrangcal@gmail.com>
2019-02-25 17:23:07 -0800

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-This is my portfolio site.
-
-Made with ReactJS.
-Styled with Styled-Components
-Uses React-Router for Multi-page functionality.
-
---
+++ precache-manifest.f6efe068365ae55b617804551fe593e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d87c8778a0206b1fb5ff94ad03d758a4",
+    "url": "/static/media/coverimg.d87c8778.JPG"
+  },
+  {
+    "revision": "f37a8f7db2ab18938de07947d2948dc9",
+    "url": "/static/media/aboutpic.f37a8f7d.JPG"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "db2ecdcf325aa640134f",
+    "url": "/static/js/main.db2ecdcf.chunk.js"
+  },
+  {
+    "revision": "b1637b8b0be31242f331",
+    "url": "/static/js/2.b1637b8b.chunk.js"
+  },
+  {
+    "revision": "db2ecdcf325aa640134f",
+    "url": "/static/css/main.5e6c297b.chunk.css"
+  },
+  {
+    "revision": "08bb48eb6978ab6345a63f53a3d79372",
+    "url": "/index.html"
+  }
+];
